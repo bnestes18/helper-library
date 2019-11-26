f00890c261c6765f0a7c27181953071ba598f56b
Brittney Estes <bnestes2014@gmail.com>
2019-11-25 22:34:21 -0500

    Refactor DOM Manipulation library to use constructor object

--- helper.js
+++ helper.js
@@ -1,47 +1,59 @@
-let helper = (function() {
+let $ = (function() {
     'use strict'
 
-    // Creates an object where public methods will live
-    let methods = {};
+    // Creates a constructor object where public methods will live
+    let Constructor = function (selector) {
+        this.selector = selector;
+    }
 
 
     // FUNCTIONS
     /*
-    Selects multiple elements as a NodeList and converts the NodeList into an array
+    Converts a NodeList into an array
     */
-    methods.createArray = function(list) {
+    Constructor.prototype.createArray = function(list) {
         return Array.prototype.slice.call(list);
     }
     /*
     Selects the first matching element in the DOM
     */
-    methods.getFirstMatchingElem = function(selector) {
-        return document.querySelector(selector);
+    Constructor.prototype.getFirstMatchingElem = function() {
+        return document.querySelectorAll(this.selector)[0];
+    }
+    /*
+    Selects the last matching element in the DOM
+    */
+   Constructor.prototype.getLastMatchingElem = function() {
+       let length = document.querySelectorAll(this.selector).length;
+       return document.querySelectorAll(this.selector)[length - 1];
     }
     /*
     Selects multiple matching elements in the DOM and 
     creates an array
     */
-    methods.getAllMatchingElems = function(selector) {
-        return this.createArray(document.querySelectorAll(selector));
+    Constructor.prototype.getAllMatchingElems = function() {
+        return this.createArray(document.querySelectorAll(this.selector));
     }
     /*
     Selects an element in the DOM and adds a class to all elements
     */
-    methods.addClass = function (elems, className) {
+    Constructor.prototype.addClass = function (className) {
+        let elems = this.getAllMatchingElems()
         elems.forEach(elem => {
             return elem.classList.add(className);
         });
     }
-    /*
-    Selects an element in the DOM and removes a class from all elements
-    */
-    methods.removeClass = function (elems, className) {
+    // /*
+    // Selects an element in the DOM and removes a class from all elements
+    // */
+    Constructor.prototype.removeClass = function (className) {
+        let elems = this.getAllMatchingElems()
         elems.forEach(elem => {
             return elem.classList.remove(className);
         });
     }
 
-    return methods;
+    return Constructor;
 
 })()
+
